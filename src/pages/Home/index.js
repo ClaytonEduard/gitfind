e0e8db00c063f699783af892b3ad4ad18b87accb
ClaytonEduard <clayton_eduard@hotmail.com>
2024-02-26 17:10:02 -0300

    Estilização da pagina home com inputs

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,11 +9,17 @@ function App() {
       <div className="conteudo">
         <img src={background} className="background" alt="background app" />
         <div className="info">
-
           <input name="usuario" placeholder="@username" />
           <button>Buscar</button>
-
-
+          <div className="perfil">
+            <img src="https://avatars.githubusercontent.com/u/11823640?v=4" className="profile" alt="Imagem de perfil"></img>
+            <div>
+              <h3>Clayton Eduard</h3>
+              <span>descricao</span>
+              <p>descricao</p>
+            </div>
+          </div>
+          <hr></hr>
         </div>
       </div>
     </div>

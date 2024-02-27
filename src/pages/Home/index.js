4daddd1f04a42523f6b7f5a70a802f863e244d64
ClaytonEduard <clayton_eduard@hotmail.com>
2024-02-27 14:17:52 -0300

    projeto finalizado com consumo restAPI GitHUB

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,36 +1,73 @@
+import { useState } from 'react';
 import Header from "../../components/Header";
 import background from '../../assets/background.png'
 import './styles.css'
 import ItemList from "../../components/ItemList";
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if (newUser.name) {
+      const { avatar_url, name, bio, login } = newUser;
+      setCurrentUser({ avatar_url, name, bio, login });
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if (newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  }
+
   return (
     <div className="App">
       <Header />
       <div className="conteudo">
         <img src={background} className="background" alt="background app" />
         <div className="info">
-          <input name="usuario" placeholder="@username" />
-          <button>Buscar</button>
-          <div className="perfil">
-            <img src="https://avatars.githubusercontent.com/u/11823640?v=4" className="profile" alt="Imagem de perfil"></img>
-            <div>
-              <h3>Clayton Eduard</h3>
-              <span>descricao</span>
-              <p>descricao</p>
+          <input type='text'
+            name="usuario"
+            value={user}
+            onChange={event => setUser(event.target.value)}
+            placeholder="@username"
+          />
+          {currentUser?.name ? (<>
+
+            <div className="perfil">
+              <img src={currentUser.avatar_url} className="profile" alt="Imagem de perfil"></img>
+              <div>
+                <h3>{currentUser.name}</h3>
+                <span>@{currentUser.login}</span>
+                <p>{currentUser.bio}</p>
+              </div>
             </div>
-          </div>
-          <hr />
-          <div>
-            <h4 className="repositorio">Repositórios</h4>
-            <ItemList title="Title1 " description='Teste de descricao' />
-            <ItemList title="Title2 " description='Teste de descricao' />
-            <ItemList title="Title3 " description='Teste de descricao' />
-          </div>
+            <hr />
+
+          </>
+          ) : null}
+          <button onClick={handleGetData}>Buscar</button>
+
+          {repos?.length ? (
+            <div>
+              <h4 className="repositorio">Repositórios</h4>
+              {repos.map(repo => (
+                (
+                  <ItemList title={repo.name} description={repo.description} />
+                )
+              ))}
+
+            </div>
+
+          ) : null}
+
         </div>
       </div>
     </div>
   );
 }
-
-export default App;
